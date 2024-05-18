6a02a4f17bc107bb062a8f0fc64670518ce26628
John Stotts <147342694+JohnStotts@users.noreply.github.com>
2024-05-18 11:37:39 -0500

    Add files via upload
    First upload complete!

---
+++ Calculator/script.js
@@ -0,0 +1,23 @@
+// calculator functions // 
+function clearDisplay() {
+    document.getElementById('display').innerText = '';
+}
+
+function deleteLast() {
+    let display = document.getElementById('display');
+    display.innerText = display.innerText.slice(0, -1);
+}
+
+function appendToDisplay(value) {
+    let display = document.getElementById('display');
+    display.innerText += value;
+}
+
+function calculateResult() {
+    let display = document.getElementById('display');
+    try {
+        display.innerText = eval(display.innerText);
+    } catch (e) {
+        display.innerText = 'Error';
+    }
+}
